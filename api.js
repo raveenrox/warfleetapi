2ca1cb299b5695f13a7cd4cee07efe2e4a51c7d7
raveenrox <raveenrox@gmail.com>
2020-03-01 13:47:11 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,16 @@
+/**
+ * My API Sandbox
+ * 
+ */
+
+// A basic route returning a canned response
+Sandbox.define('/hello', 'get', function(req, res) {
+    // send 'Hello world' response
+    res.send('Hello world');
+});
+
+
+
+
+
+
